9c61c4a0a57c34becf917c60d9779af077984c77
M4V3R1CK <jonathangrismond@gmail.com>
2023-09-02 10:27:09 -0300

    basic functions
    basic functions

--- account.py
+++ account.py
@@ -8,3 +8,19 @@ class Account:
         self.__holder = holder
         self.__balance = balance
         self.__limit = limit
+
+    def statement(self):
+        print("The Balance is {} from Holder {}".format(
+            self.__balance,
+            self.__holder
+        ))
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        self.__balance -= amount
+
+    def transfer(self, amount, destination):
+        self.withdraw(amount)
+        destination.deposit(amount)
