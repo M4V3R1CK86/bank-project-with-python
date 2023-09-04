754594dcdfd14377727d8830fa16c386c54d7103
M4V3R1CK <jonathangrismond@gmail.com>
2023-09-04 01:29:19 -0300

    Create Label
    Create a new Label widget and set its text to "Hello world"

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from PyQt5.QtWidgets import *
 
 def main():
 
-    app = QApplication([])
-    window = QWidget()
+    app = QApplication([])    # Creating app with an empty list
+    window = QWidget()      # Creating app window
+    # Create a new Label widget and set its text to "Hello world"
+    label1 = QLabel(window)
+    label1.setText("Hello World")
+    # putting the window running
     window.show()
     app.exec_()
 
