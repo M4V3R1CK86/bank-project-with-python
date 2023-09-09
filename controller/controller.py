9d2e0e22a9ce35e7baa3f46ca30ea0808032cf71
M4V3R1CK <jonathangrismond@gmail.com>
2023-09-09 15:08:04 -0300

    update controller
    calling login_view.py after 1 second of loading_view.py opened.

--- controller/controller.py
+++ controller/controller.py
@@ -1,4 +1,7 @@
+from PyQt6.QtCore import QTimer
+
 from view.loading_view import LoadingView
+from view.login_view import LoginView
 
 
 class ScreenController:
@@ -14,3 +17,18 @@ class ScreenController:
 
         # Show the loading view, making it visible to the user.
         self.loading_view.show()
+        # Create a QTimer instance and schedule a single-shot timer event to call the self.show_login_view() function after 1000 milliseconds (1 second).
+
+        QTimer().singleShot(1000, self.show_login_view)
+
+    def show_login_view(self):
+        # Define a method called show_login_view that belongs to the current class.
+
+        self.login_view = LoginView()
+        # Create an instance of the LoginView class and assign it to the instance variable self.login_view.
+
+        self.login_view.show()
+        # Show the login_view window.
+
+        self.loading_view.close()
+        # Close the loading_view window.
