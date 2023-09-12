990ce5b685c07c431347e38c64f05fa96c30ad78
M4V3R1CK <jonathangrismond@gmail.com>
2023-09-12 02:55:20 -0300

    fuction logging_in
    function to call.... field validation functions and screen color change

--- view/login_view.py
+++ view/login_view.py
@@ -1,3 +1,5 @@
+import re  # Módulo para validação de e-mail
+
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QCursor
 from PyQt6.QtWidgets import QLabel, QLineEdit, QMessageBox, QPushButton
@@ -14,24 +16,29 @@ class LoginView(BaseView):
         x = (width_screen - width_input) // 2
         x2 = (width_screen - width_btn) // 2
 
-        styleSheet_input = "background-color: #967978; color: #c9a9a8; border-radius: 10px; padding-left: 10px;"
-        styleSheet_login_btn = "background-color: #967978; color: #c9a9a8; border-radius: 10px;"
-        styleSheet_create_account_btn = "background: none; border: none; color: #c9a9a8;"
+        styleSheet_input = "background-color: #331e1e; color: #ffffe8; border-radius: 10px; padding-left: 10px;"
+        styleSheet_login_btn = "background-color: #331e1e; color: #ffffe8; border-radius: 10px;"
+        styleSheet_create_account_btn = "background: none; border: none; color: #fea509;"
 
         PointingHandCursor = QCursor(Qt.CursorShape.PointingHandCursor)
 
         # Create a title label
         title_label = QLabel("DarkStar Bank", self)
-        title_label.setStyleSheet("color: #c9a9a8; font-size: 50px;")
+        title_label.setStyleSheet("color: #ffffe8; font-size: 50px;")
         title_label.adjustSize()
         title_width = title_label.width()
         title_label.move((width_screen - title_width) // 2, 250)
 
+        self.email_input = QLineEdit(self)
+        self.email_input.setPlaceholderText("E-mail")
+        self.email_input.setGeometry(x, 320, width_input, 30)
+        self.email_input.setStyleSheet(styleSheet_input)
+
         # Create a username input field
-        self.username_input = QLineEdit(self)
-        self.username_input.setPlaceholderText("User")
-        self.username_input.setGeometry(x, 320, width_input, 30)
-        self.username_input.setStyleSheet(styleSheet_input)
+        # self.username_input = QLineEdit(self)
+        # self.username_input.setPlaceholderText("User")
+        # self.username_input.setGeometry(x, 320, width_input, 30)
+        # self.username_input.setStyleSheet(styleSheet_input)
 
         # Create a password input field
         self.password_input = QLineEdit(self)
@@ -46,7 +53,7 @@ class LoginView(BaseView):
         login_btn.setStyleSheet(styleSheet_login_btn)
         login_btn.setCursor(PointingHandCursor)
 
-        login_btn.clicked.connect(self.validate_login)
+        login_btn.clicked.connect(self.logging_in)
 
         # Create a Create Account button
         create_account_btn = QPushButton("Create Account", self)
@@ -57,7 +64,54 @@ class LoginView(BaseView):
         # Connect the Create Account button to a slot method
         create_account_btn.clicked.connect(self.on_create_account_btn_clicked)
 
-    # Set the controller instance
+    # Get the email and password from input fields
+    def logging_in(self):
+        email = self.email_input.text().strip()
+        password = self.password_input.text().strip()
+
+        # Perform data validation
+        if not self.is_valid_input(email, password):
+            return  # Exit if validation fails
+
+        # Call the logging_in method in the controller, passing the data
+        if self.controller:
+            self.controller.logging_in(email, password)
+
+        # Clear the input fields
+        self.email_input.setText("")
+        self.password_input.setText("")
+
+    def is_valid_input(self, email, password):
+
+        # Perform data validation here and display error messages as needed
+        if not email:
+            self.show_error_message("Please enter your Email.")
+            return False
+
+        elif not self.is_valid_email(email):
+            self.show_error_message("Invalid email format.")
+            return False
+
+        elif len(password) < 6:
+            self.show_error_message(
+                "Password must be at least 6 characters long.")
+            return False
+
+        return True
+
+    def is_valid_email(self, email):
+
+        # Use a regular expression to validate the email format
+        email_pattern = r'^\S+@\S+\.\S+$'
+        return re.match(email_pattern, email) is not None
+
+    def show_error_message(self, message):
+        msg = QMessageBox()
+        msg.setWindowTitle("Error")
+        msg.setText(message)
+        msg.setIcon(QMessageBox.Icon.Critical)
+        msg.exec()
+
     def set_controller(self, controller):
         self.controller = controller
 
@@ -65,39 +119,3 @@ class LoginView(BaseView):
     def on_create_account_btn_clicked(self):
         if self.controller:
             self.controller.show_create_account_view()
-
-    # Get the username and password from input fields
-    def validate_login(self):
-        username = self.username_input.text()
-        password = self.password_input.text()
-
-        # Check if username or password are empty
-        if not username or not password:
-            msg = QMessageBox()
-            msg.setWindowTitle("Error")
-            msg.setText("Please fill in all fields.")
-            msg.setIcon(QMessageBox.Icon.Critical)
-            msg.exec()
-            msg.move(self.geometry().center())
-
-        # Check if the password length is less than 6 characters
-        elif len(password) < 6:
-            msg = QMessageBox()
-            msg.setWindowTitle("Error")
-            msg.setText("Password must be at least 6 characters long.")
-            msg.setIcon(QMessageBox.Icon.Critical)
-            msg.exec()
-            msg.move(self.geometry().center())
-
-        # Check if username and password are correct
-        elif username != "correct_user" or password != "correct_password":
-            msg = QMessageBox()
-            msg.setWindowTitle("Error")
-            msg.setText("Incorrect username or password.")
-            msg.setIcon(QMessageBox.Icon.Critical)
-            msg.exec()
-            msg.move(self.geometry().center())
-
-        else:
-            # If validation is successful, close the login window
-            self.close()
