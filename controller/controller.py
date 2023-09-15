2393fb0e044c6ca9f45ad79f89aba8fc8eb7458c
M4V3R1CK <jonathangrismond@gmail.com>
2023-09-15 06:20:23 -0300

    updating methods to work with hash
    commenting all controller lines and updating create_accoun and loggin_in to work with the hashed password

--- controller/controller.py
+++ controller/controller.py
@@ -1,11 +1,12 @@
+import bcrypt
 from PyQt6.QtCore import QTimer
 from PyQt6.QtWidgets import QMessageBox
 
-from model.account_model import AccountModel
 from model.database_config_model import DatabaseConfigModel
 from model.database_manager import DatabaseManager
+from model.users_model import UsersModel
 from view.base_view import BaseView
-from view.creat_account_view import CreateAccountView
+from view.create_account_view import CreateAccountView
 from view.home_view import HomeView
 from view.loading_view import LoadingView
 from view.login_view import LoginView
@@ -13,19 +14,20 @@ from view.login_view import LoginView
 
 class ScreenController:
     def __init__(self, app):
-
-        # Constructor for the ScreenController class. It receives an instance of QApplication as an argument.
+        # Constructor for the ScreenController class.
+        # It receives an instance of QApplication as an argument.
         self.app = app
 
-        # Create an instance of DatabaseConfigModel
+        # Create an instance of DatabaseConfigModel to handle database configuration.
         self.db_config_model = DatabaseConfigModel()
 
-        # Create an instance of DatabaseManager
+        # Create an instance of DatabaseManager to manage database operations.
         self.database_manager = DatabaseManager(self.db_config_model)
 
+        # Create an instance of CreateAccountView.
         self.create_account_view = CreateAccountView()
 
-        # Configure a conexão entre a view e o controller
+        # Configure the connection between the view and the controller.
         self.create_account_view.set_controller(self)
 
     def show_loading_view(self):
@@ -37,65 +39,109 @@ class ScreenController:
         # Show the loading view, making it visible to the user.
         self.loading_view.show()
 
-        # Create a QTimer instance and schedule a single-shot timer event to call the self.show_login_view() function after 1000 milliseconds (1 second).
-        QTimer().singleShot(5000, self.show_login_view)
+        # Create a QTimer instance and schedule a single-shot timer event
+        # to call the self.show_login_view() function
+        #  after 2000 milliseconds (2 seconds).
+        QTimer().singleShot(2000, self.show_login_view)
 
     def show_login_view(self):
+        # Method to show the login view.
 
+        # Create an instance of the LoginView class.
         self.login_view = LoginView()
-        self.login_view.controller = self  # Pass the controller instance
+
+        # Pass the controller instance to the login view.
+        self.login_view.controller = self
+
+        # Show the login view.
         self.login_view.show()
-        self.loading_view.close()  # Close the loading_view window.
+
+        # Close the loading_view window.
+        self.loading_view.close()
 
     def show_create_account_view(self):
+        # Method to show the create account view.
+
+        # Create an instance of the CreateAccountView class.
         self.create_account_view = CreateAccountView()
+
+        # Set the controller for the create account view.
         self.create_account_view.set_controller(self)
+
+        # Show the create account view.
         self.create_account_view.show()
-        self.login_view.close()  # Close the login_view window.
+
+        # Close the login_view window.
+        self.login_view.close()
 
     def create_account(self, first_name, last_name, email, password):
+        # Method to create a new user account.
 
-        # Create an AccountModel instance with the provided data
-        account_model = AccountModel(first_name, last_name, email, password)
+        # Create an instance of UsersModel with user information.
+        users_model = UsersModel(first_name, last_name, email)
 
-        # Attempt to save the account data to the database
-        if self.database_manager.save_account(account_model):
+        # Set the hashed password for the user.
+        users_model.set_password(password)
+
+        # Attempt to save the user account in the database.
+        if self.database_manager.save_account(users_model):
+            # Display a success message if the account is created and saved.
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Icon.Information)
             msg.setText("Account created successfully!!")
-            msg.setWindowTitle("success!")
+            msg.setWindowTitle("Success!")
             msg.setStandardButtons(QMessageBox.StandardButton.Ok)
             msg.exec()
-            QTimer.singleShot(200, self.close_create_account_and_show_login)
 
+            # Close the create_account_view and show the login view after a delay.
+            QTimer.singleShot(200, self.close_create_account_and_show_login)
         else:
-            # Handle the case where saving to the database failed
+            # Display an error message if the account creation fails.
             print('Failed to create and save the account.')
 
     def logging_in(self, email, password):
+        # Method to handle user login.
 
-        # Chame are_credentials_valid com db_params como argumento
-        if self.database_manager.are_credentials_valid(email, password):
-            # Credenciais válidas, exiba a HomeView
-            self.show_home_view()
+        # Check if the provided email and password are valid credentials.
+        user_data = self.database_manager.are_credentials_valid(
+            email, password)
+
+        if user_data:
+            # If valid credentials, extract user information.
+            user_id, first_name, last_name = user_data
+
+            # Show the home view for the authenticated user.
+            self.show_home_view(user_id, first_name, last_name)
+
+            # Close the login_view.
             self.login_view.close()
         else:
-            # Credenciais inválidas, exiba uma mensagem de erro
+            # Show an error message for invalid credentials.
             self.login_view.show_error_message("Invalid email or password")
 
     def close_create_account_and_show_login(self):
-        # Fecha a janela de create_account
+        # Method to close the create_account_view and show the login view.
+
+        # Close the create_account_view.
         self.create_account_view.close()
 
-        # Mostra a tela de login
+        # Show the login view.
         self.show_login_view()
 
-    def show_home_view(self):
+    def show_home_view(self, user_id, first_name, last_name):
+        # Method to show the home view for an authenticated user.
 
-        self.home_view = HomeView()
+        # Create an instance of HomeView with user information.
+        self.home_view = HomeView(user_id, first_name, last_name)
+
+        # Show the home view.
         self.home_view.show()
 
     def show_base_view(self):
+        # Method to show the base view.
 
+        # Create an instance of BaseView.
         self.base_view = BaseView()
+
+        # Show the base view.
         self.base_view.show()
